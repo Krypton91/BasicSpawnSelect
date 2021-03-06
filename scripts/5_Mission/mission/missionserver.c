9a743b6af0564f98522d67f21bfaa70d72b59ac9
Krypton91 <krypton1@gmx.at>
2021-03-06 20:44:34 +0100

    Fixed wrong player creating (spawnmode 1).

--- scripts/5_Mission/mission/missionserver.c
+++ scripts/5_Mission/mission/missionserver.c
@@ -3,27 +3,24 @@ modded class MissionServer extends MissionBase
 
 	override PlayerBase OnClientNewEvent(PlayerIdentity identity, vector pos, ParamsReadContext ctx)
 	{
-		if(GetSpawnSelectServer().GetCurrentSpawnsystemMode() == 1)
-		{
-			string characterType;
-			SyncRespawnModeInfo(identity);
+		string characterType;
+		SyncRespawnModeInfo(identity);
 
-			if(ProcessLoginData(ctx) && (m_RespawnMode == GameConstants.RESPAWN_MODE_CUSTOM) && GetGame().ListAvailableCharacters().Find(GetGame().GetMenuDefaultCharacterData().GetCharacterType()) > -1 && !GetGame().GetMenuDefaultCharacterData(false).IsRandomCharacterForced())
-			{
-				characterType = GetGame().GetMenuDefaultCharacterData().GetCharacterType();
-			}
-			else
-			{
-				characterType = GetGame().CreateRandomPlayer();
-				GetGame().GetMenuDefaultCharacterData().GenerateRandomEquip();
-			}
-			if(CreateCharacter(identity, pos, ctx, characterType))
-			{
-				EquipCharacter(GetGame().GetMenuDefaultCharacterData());
-				GetSpawnSelectServer().SendClientToSpawnSelectMenu(identity);
-			}
-			return m_player;
+		if(ProcessLoginData(ctx) && (m_RespawnMode == GameConstants.RESPAWN_MODE_CUSTOM) && GetGame().ListAvailableCharacters().Find(GetGame().GetMenuDefaultCharacterData().GetCharacterType()) > -1 && !GetGame().GetMenuDefaultCharacterData(false).IsRandomCharacterForced())
+		{
+			characterType = GetGame().GetMenuDefaultCharacterData().GetCharacterType();
 		}
-		return super.OnClientNewEvent(identity, pos, ctx);
+		else
+		{
+			characterType = GetGame().CreateRandomPlayer();
+			GetGame().GetMenuDefaultCharacterData().GenerateRandomEquip();
+		}
+		if(CreateCharacter(identity, pos, ctx, characterType))
+		{
+			EquipCharacter(GetGame().GetMenuDefaultCharacterData());
+			if(GetSpawnSelectServer().GetCurrentSpawnsystemMode() == 1)
+				GetSpawnSelectServer().SendClientToSpawnSelectMenu(identity);
+		}
+		return m_player;
 	}
 };
